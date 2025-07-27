7fe95bf2a1f874eb9b4277629df84cb90dae2287
olmnlo <hosamag54@gmail.com>
2025-07-27 10:18:31 +0300

    first commit

---
+++ capstoneProject1/src/main/java/org/example/capstoneproject1/CapstoneProject1Application.java
@@ -0,0 +1,13 @@
+package org.example.capstoneproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneProject1Application.class, args);
+    }
+
+}
